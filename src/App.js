6677a1cb1eaf070cfa5d6a70c78b5d7663f35f08
rahatsikz <rascal6271@gmail.com>
2023-02-09 00:44:09 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./routes/router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/router.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Services from "../Components/Services/Services";
+import Main from "../layouts/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "services",
+        element: <Services></Services>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);

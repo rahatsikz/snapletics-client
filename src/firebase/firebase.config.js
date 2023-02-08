867d75fbe7d8a1a00a65d05ddd1511a2c3b21fb8
rahatsikz <rascal6271@gmail.com>
2023-02-09 00:05:27 +0600

    project created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAFHCh1EqHuGLHG7N1gP3sWoV0zjHqf5O8",
+  authDomain: "snapletics-rat.firebaseapp.com",
+  projectId: "snapletics-rat",
+  storageBucket: "snapletics-rat.appspot.com",
+  messagingSenderId: "317228205934",
+  appId: "1:317228205934:web:2d569ffed77a08942da37f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
